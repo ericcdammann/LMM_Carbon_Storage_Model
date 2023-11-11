cd532e6a1fef32445c0ac97489b0053bb46abc03
Eric Dammann <ericcdammann@gmail.com>
2023-11-11 15:54:18 -0500

    added code files

--- main_code.jl
+++ main_code.jl
@@ -11,7 +11,7 @@ include("marsh_bed_shear_stress.jl")
 # Computational Parameters
 
 years = 150
-n_iter = 100000
+n_iter = 1000
 t_init = 0
 t_fin = years*365*24*60*60
 h = (t_fin-t_init)/n_iter
@@ -26,16 +26,10 @@ hm_init = 0.2602
 hl_init = 3.2786
 wb_init = wl_init+wm_init
 
-Ctot_init = 0
 ctot_init = 0
-Clab_init = 0
-clab_init = 0
-Cref_init = 0
-cref_init = 0
-torg_init = 0
-Mtot_init = 0
-mtot_init = 0
-closs_init = 0
+clab_init = 1.222
+cref_init = 1.222
+cdec_init = 0
 
 # Model Paramters 
 
@@ -48,270 +42,234 @@ wind = 6
 ka = 2
 ke = 0.1/(365*24*3600)
 amp = 1.4/2
-RSLR = 5*(10^-3)/(3600*24*365)
+RSLR = [1 3 5 7]*(10^-3)/(3600*24*365)
 rhom = 1000
 lamda = 0.0001
 beta = 10^10
-k = 0.012/(24*60*60)
-wtidal = 15000
+kk = 0.012/(24*60*60)
+wtidal = 15*wm_init
 
 # Preallocating Arrays
 
-WL = zeros(n_iter)
-WL[1] = wl_init
-HM = zeros(n_iter)
-HM[1] = hm_init
-HL = zeros(n_iter)
-HL[1] = hl_init
-WB = zeros(n_iter)
-WB[1] = wb_init
+WL = zeros(n_iter,length(RSLR))
+HM = zeros(n_iter,length(RSLR))
+HL = zeros(n_iter,length(RSLR))
+WB = zeros(n_iter,length(RSLR))
 
-CTOT = zeros(n_iter)
-CTOT[1] = Ctot_init
-cTOT = zeros(n_iter)
-cTOT[1] = ctot_init
-CLAB = zeros(n_iter)
-CLAB[1] = Clab_init
-cLAB = zeros(n_iter)
-cLAB[1] = clab_init
-CREF = zeros(n_iter)
-CREF[1] = Cref_init
-cREF = zeros(n_iter)
-cREF[1] = cref_init
-TORG = zeros(n_iter)
-TORG[1] = torg_init
-MTOT = zeros(n_iter)
-MTOT[1] = Mtot_init
-mTOT = zeros(n_iter)
-mTOT[1] = mtot_init
-cLOSS = zeros(n_iter)
-cLOSS[1] = closs_init
+CTOT = zeros(n_iter,length(RSLR))
+CLAB = zeros(n_iter,length(RSLR))
+CREF = zeros(n_iter,length(RSLR))
 
-for i in 1:n_iter-1
+MFLUX = zeros(n_iter,length(RSLR))
+MTOT = zeros(n_iter,length(RSLR))
 
-    fetch = WL[i]
-    hl = HL[i]
-    hm = HM[i]
-    wb = WB[i]
-    wm = wb-fetch
+for i in eachindex(RSLR)
 
-    Ctot = CTOT[i]
-    ctot = cTOT[i]
-    Clab = CLAB[i]
-    clab = cLAB[i]
-    Cref = CREF[i]
-    cref = cREF[i]
-    torg = TORG[i]
-    Mtot = MTOT[i]
-    mtot = mTOT[i]
-    closs = cLOSS[i]
+    WL[1,i] = wl_init
+    HM[1,i] = hm_init
+    HL[1,i] = hl_init
+    WB[1,i] = wb_init
 
-    # Organic Sediment Production
-
-    BMax = 2.500
-    Dmin = 0
-    Dmax = 0.7167*2*amp-0.483
-    AA = 0.25*(-Dmin-Dmax)*(Dmax-3*Dmin)
-    Bpeak = BMax*(hm-Dmax)*(hm-Dmin)/AA
-    
-    if Bpeak <= 1e-3
-        
-        Bpeak = 0
-    
-    end
-    
-    Bfrac = (Bpeak/BMax)
-    nuGp = 0.0138
-    AMC = (365/2)*Bpeak*(nuGp)/(365*24*60*60)
-    por = 1000/2650
-    chiref = 0.15
-    Rref = AMC*chiref
-    po = 1000 
-    O = (1/por)*(Rref/po)
-    
-    # Average Depths
-    
-    fac = min(1,hl/(2*amp))
-    fac2 = min(1,hm/(2*amp))
-    Hf = (hl+(hl-fac*2*amp))/2
-    Hm = (hm+(hm-fac2*2*amp))/2
-    
-    # Sediment Concentrations
-    
-        # Sediment Concentration in Lagoon
-    
-    tau_1 =  lagoon_bed_shear_stress(fetch,wind,Hf)
-    tau_lagoon = max((tau_1-tcr)/tcr,0)*lamda
-    Cr = rhos*tau_lagoon/(1+tau_lagoon)
-    
-        # Sediment Concentration in Marsh
-    
-    if Hm > 1e-4
-    
-        tau_2 =  marsh_bed_shear_stress(fetch,wind,Hm,Bfrac)
-    
-    else 
-    
-        tau_2 = 0
-    
-    end
-    
-    tau_marsh = max((tau_2-tcr)/tcr,0)*lamda
-    Cm = rhos*tau_marsh/(1+tau_marsh)
-    
-    # Sediment Fluxes
-    
-        # Flux from Lagoon to Marsh
-    
-    Fm = (Cr-Cm)*min(2*amp,hm)/P/rhom
-    
-        # Flux from Open Ocean to Lagoon
-    
-    Fc = (Cr-Co)*(fac*2*amp)/P/rhom
-    
-    # Lagoon/Marsh Edge
-    
-        # Marsh Erosion Rate
-    
-    dist = 10
-    hs = hm+(hl-hm)*(1-exp(-dist*0.1/hl));
-    W = wave_power(amp,wind,fetch,hs)
-    Be = ke*W/(hs-hm)
-    
-        # Marsh Progradation Rate
-    
-    Ba = ka*Cr*ws/rhom
-    
-    # Upland Migration
-    
-    Umig = (Fm+O)/beta
-
-    # Carbon Budget
-    
-    Rae = ((Ba-Be)+Umig)*po*por*torg
-    chilab = 1-chiref
-    Rlab  = AMC*chilab
-    phi = wm/(2*wtidal)
-    
-    # Defining Equations
-
-        # Morphodynamic Outputs
-    
-    dWLdt = Be-Ba
-    dHMdt = -Fm-O+RSLR
-    dHLdt = -(Be-Ba)*(hl-hm)/fetch+Fm*wm/fetch+Fc+RSLR
-    dWBdt = Umig
-
-        # Carbon Dynamics Outputs
-
-    dCTOTdt = AMC*wm
-    dcTOTdt = AMC
-    dCLABdt = Rlab*wm-k*Clab-Rae
-    dcLABdt = Rlab-k*clab
-    dCREFdt = Rref*wm-Rae
-    dcREFdt = Rref
-    dTORGdt = O
-    dMTOTdt = k*Clab*phi
-    dmTOTdt = k*clab*phi
-    dcLOSSdt = k*clab
-
-    # Foward Euler Scheme
-
-    WL[i+1] = fetch+h*dWLdt
-    HM[i+1] = hm+h*dHMdt
-    HL[i+1] = hl+h*dHLdt
-    WB[i+1] = wb+h*dWBdt
-
-    CTOT[i+1] = Ctot+h*dCTOTdt
-    cTOT[i+1] = ctot+h*dcTOTdt
-    CLAB[i+1] = Clab+h*dCLABdt
-    cLAB[i+1] = clab+h*dcLABdt
-    CREF[i+1] = Cref+h*dCREFdt
-    cREF[i+1] = cref+h*dcREFdt
-    TORG[i+1] = torg+h*dTORGdt
-    MTOT[i+1] = Mtot+h*dMTOTdt
-    mTOT[i+1] = mtot+h*dmTOTdt
-    cLOSS[i+1] = closs+h*dcLOSSdt
+    CTOT[1,i] = ctot_init
+    CLAB[1,i] = clab_init
+    CREF[1,i] = cref_init
 
 end
 
-# Width of the Marsh
+for j in eachindex(RSLR)
 
-WM = WB-WL
+    for k in 1:n_iter-1
 
-# Mass Balance
+        fetch = WL[k,j]
+        hl = HL[k,j]
+        hm = HM[k,j]
+        wb = WB[k,j]
+        wm = wb-fetch
 
-mass_balance = cTOT-(cLAB+cREF+cLOSS)
+        ctot = CTOT[k,j]
+        clab = CLAB[k,j]
+        cref = CREF[k,j]
+        mtot = MTOT[k,j]
 
-# Plotting Results
+        # Organic Sediment Production
 
-# Morphodynamic Outputs [L]
+        BMax = 2.500
+        Dmin = 0
+        Dmax = 0.7167*2*amp-0.483
+        AA = 0.25*(-Dmin-Dmax)*(Dmax-3*Dmin)
+        Bpeak = BMax*(hm-Dmax)*(hm-Dmin)/AA
+    
+        if Bpeak <= 1e-3
+        
+            Bpeak = 0
+    
+        end
+    
+        Bfrac = (Bpeak/BMax)
+        nuGp = 0.0138
+        AMC = (365/2)*Bpeak*(nuGp)/(365*24*60*60)
+        por = 1000/2650
+        chiref = 0.15
+        Rref = AMC*chiref
+        po = 1000 
+        O = (1/por)*(Rref/po)
+    
+        # Average Depths
+    
+        fac = min(1,hl/(2*amp))
+        fac2 = min(1,hm/(2*amp))
+        Hf = (hl+(hl-fac*2*amp))/2
+        Hm = (hm+(hm-fac2*2*amp))/2
+    
+        # Sediment Concentrations
+    
+            # Sediment Concentration in Lagoon
+    
+        tau_1 =  lagoon_bed_shear_stress(fetch,wind,Hf)
+        tau_lagoon = max((tau_1-tcr)/tcr,0)*lamda
+        Cr = rhos*tau_lagoon/(1+tau_lagoon)
+    
+            # Sediment Concentration in Marsh
+    
+        if Hm > 1e-4
+    
+            tau_2 =  marsh_bed_shear_stress(fetch,wind,Hm,Bfrac)
+    
+        else 
+    
+            tau_2 = 0
+    
+        end
+    
+        tau_marsh = max((tau_2-tcr)/tcr,0)*lamda
+        Cm = rhos*tau_marsh/(1+tau_marsh)
+    
+        # Sediment Fluxes
+    
+            # Flux from Lagoon to Marsh
+    
+        Fm = (Cr-Cm)*min(2*amp,hm)/P/rhom
+    
+            # Flux from Open Ocean to Lagoon
+    
+        Fc = (Cr-Co)*(fac*2*amp)/P/rhom
+    
+        # Lagoon/Marsh Edge
+    
+            # Marsh Erosion Rate
+    
+        dist = 10
+        hs = hm+(hl-hm)*(1-exp(-dist*0.1/hl));
+        W = wave_power(amp,wind,fetch,hs)
+        Be = ke*W/(hs-hm)
+    
+            # Marsh Progradation Rate
+    
+        Ba = ka*Cr*ws/rhom
+    
+        # Upland Migration
+    
+        Umig = (Fm+O)/beta
 
+        # Carbon Decomposition
+
+        chilab = 1-chiref
+        Rlab  = AMC*chilab
+        phi = wm/(2*wtidal)
+        
+        # Carbon Dynamics Outputs
+
+        dCTOTdt = AMC-(kk*clab)
+        dCLABdt = Rlab-(kk*clab)
+        dCREFdt = Rref
+        mflux = phi*kk*clab
+
+        # Morphodynamic Outputs
+    
+        dWLdt = Be-Ba
+        dHMdt = -Fm-O+RSLR[j]
+        dHLdt = -(Be-Ba)*(hl-hm)/fetch+Fm*wm/fetch+Fc+RSLR[j]
+        dWBdt = Umig
+
+        # Foward Euler Scheme
+
+        WL[k+1,j] = fetch+h*dWLdt
+        HM[k+1,j] = hm+h*dHMdt
+        HL[k+1,j] = hl+h*dHLdt
+        WB[k+1,j] = wb+h*dWBdt
+
+        CTOT[k+1,j] = ctot+h*dCTOTdt
+        CLAB[k+1,j] = clab+h*dCLABdt
+        CREF[k+1,j] = cref+h*dCREFdt
+        MTOT[k+1,j] = mtot+h*mflux*wm
+
+        MFLUX[k,j] = mflux 
+        MFLUX[k+1,j] = mflux
+
+    end
+
+end
+
+mass_balance = CTOT-(CLAB+CREF).+(clab_init+cref_init)
+
+# Morphodynamics
+
+WM = WB - WL 
 p1_md = plot(t,WL)
 p2_md = plot(t,WM)
 p3_md = plot(t,HL)
 p4_md = plot(t,HM)
 
-display(plot(p1_md,p2_md,p3_md,p4_md, layout = 4, 
-plot_title="Morphodynamic Outputs", 
-label=["wl" "wm" "hl" "hm"],
+fig1=plot(p1_md,p2_md,p3_md,p4_md, layout = 4, 
+title=["Width of Lagoon (wl)" "Width of Marsh (wl)" "Depth of Lagoon (hl)" "Depth of Marsh below MHW (hm)" ] ,
+label=["RSLR = 1 mm/year" "RSLR = 3 mm/year" "RSLR = 5 mm/year" "RSLR = 7 mm/year"],
 xlabel="Years",
-ylabel="Meters"
-))
+ylabel="Meters",
+size=(1000,1000)
+)
+display(fig1)
+savefig(fig1,"morphodynamics1.pdf")
 
-# Carbon Dynamics Outputs [M * L^-1]
+p1_cd = plot(t,CREF)
+p2_cd = plot(t,CLAB)
 
-p1_cd1 = plot(t,CTOT)
-p2_cd1 = plot(t,CLAB)
-p3_cd1 = plot(t,CREF)
-p4_cd1 = plot(t,MTOT)
-
-display(plot(p1_cd1,p2_cd1,p3_cd1,p4_cd1, layout = 4,
-plot_title="Carbon Dynamics Outputs [M*L^-1]", 
-label=["Ctot" "Clab" "Cref" "Mtot"],
+fig2 = plot(p1_cd,p2_cd, layout=2,
+title=["Refractory Carbon Pool (Cref)" "Labile Carbon Pool (Clab)"],
+label=["RSLR = 1 mm/year" "RSLR = 3 mm/year" "RSLR = 5 mm/year" "RSLR = 7 mm/year"],
 xlabel="Years",
-ylabel="kg per meter"
-))
+ylabel="kgs of Carbon per m^2",
+legend=:outerbottom,
+size=(700,500)
+)
+display(fig2)
+savefig(fig2,"carbonstorage1.pdf")
 
-# Carbon Dynamics Outputs [M * L^-2]
+MFLUX = MFLUX*(1.0*10^6*3600)
+leg_ent = ["RSLR = $(RSLR[l]/((10^-3)/(3600*24*365))) mm/yr" for l in eachindex(RSLR)]
+fig3=plot(title="CH4 Flux over Time", legend=:outerright)
+for m in eachindex(RSLR)
+    plot!(fig3,t,MFLUX[:,m],label=leg_ent[m])
+end
+xlabel!("Years")
+ylabel!("mgs of CH4 per m^2 per hour")
+up_bound = t*0 .+27.4
+low_bound = t*0 .+8
+plot!(fig3,t,up_bound,linecolor=:black,linestyle=:dash,label="Comer-Warner et al. 2022")
+plot!(fig3,t,low_bound,linecolor=:black,linestyle=:dash,label=false)
+display(fig3)
+savefig(fig3,"ch4flux1.pdf")
 
-p1_cd2 = plot(t,cTOT)
-p2_cd2 = plot(t,cLAB)
-p3_cd2 = plot(t,cREF)
-p4_cd2 = plot(t,mTOT)
+fig4=plot(t,MTOT,
+title="Total Methane Emissions (Mtot)",
+label=["RSLR = 1 mm/year" "RSLR = 3 mm/year" "RSLR = 5 mm/year" "RSLR = 7 mm/year"]
+)
+xlabel!("Years")
+ylabel!("kgs of CH4")
+display(fig4)
+savefig(fig4,"ch4emissions1.pdf")
 
-display(plot(p1_cd2,p2_cd2,p3_cd2,p4_cd2, layout = 4,
-plot_title="Carbon Dynamics Outputs [M*L^-2]", 
-label=["ctot" "clab" "cref" "mtot"],
-xlabel="Years",
-ylabel="kg per meter^2"
-))
-
-# Mass Balance
-
-display(plot(t,mass_balance,
-plot_title="Mass Balance", 
-label=false,
-xlabel="Years",
-ylabel="Error"
-))
-
-# Average Methane Flux (mg per m^2 per hr)
-
-mflux1 = MTOT[n_iter]/mean(WM)/years
-
-a = mflux1*(1.0*10^6)/(365*24)
-
-mflux2 =mTOT[n_iter]/years
-
-b = mflux2*(1.0*10^6)/(365*24)
-
-c = cREF[n_iter]/years
-
-d = [a b c years wtidal]
-
-print(d)
-
-# 8.2 to 27.4 mg CH4 per m^2 per hour
-
-# 0.21745274 kg C per m^2 per year
+average_mflux = zeros(1,length(RSLR))
+for n in eachindex(RSLR)
+    average_mflux[n] = mean(MFLUX[:,n])
+end
+print(average_mflux)
